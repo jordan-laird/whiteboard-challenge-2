1d7eb32e220d667e1700dfcffe86ba7d7b838c31
jordan-laird <jordanmack.laird@gmail.com>
2018-10-23 11:32:17 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,48 @@
 # Enter your procedural solution here!
+
+require 'pry'
+
+def collect_multiples(limit)
+    collected_array = []
+
+    limit.times do |n|
+    # for n in 1..1000
+        if  n != 0 && (n % 3 == 0 || n % 5 == 0) 
+            collected_array << n
+            n += 1
+
+        else
+            n += 1
+        end
+
+    end
+
+    collected_array
+
+   
+end
+
+def sum_multiples(limit)
+    final_sum = 0
+
+    limit.times do |n|
+    # for n in 1..1000
+        if  n != 0 && (n % 3 == 0 || n % 5 == 0) 
+            final_sum += n
+            n += 1
+
+        else
+            n += 1
+        end
+
+    end
+
+    final_sum
+
+   
+end
+
+
+
+
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,42 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+
+        collected_array = []
+        @limit.times do |n|
+            if n != 0 && (n%3 == 0 || n % 5 == 0)
+                collected_array << n
+                n += 1
+            else
+                n +=1
+            end
+        end
+        collected_array
+    end
+
+    def sum_multiples
+        final_sum = 0
+
+        @limit.times do |n|
+            if n != 0 && (n % 3 == 0 || n % 5 == 0)
+                final_sum += n
+                n += 1
+            else
+                n += 1
+            end
+        end
+        final_sum
+    end
+
+
+
+    
+
+end
